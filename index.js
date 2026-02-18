922c533c28ea56e9fccac062e7ea42419dcee0ca
adminkemendikbudristek <admin@kemendikbudristek.com>
2026-02-18 16:36:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res) => {
   console.log("Webhook masuk:");
   console.log(req.body);
-  res.status(200).send("OK");
+
+  res.status(200).json({
+    status: "received"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
